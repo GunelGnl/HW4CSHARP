0ca2652f9204b1d5940291dd7cf4b1a3db7f5078
Gunel <kocharlig@gmail.com>
2023-04-22 15:55:45 +0400

    Done all 3 task

---
+++ eX1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Task25
+
+System.Console.WriteLine("Add digit A");
+int A = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Add digit B");
+int B = int.Parse (Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+   result = result* A; 
+}
+System.Console.WriteLine("Final answer:" + result);
+
+//Task27
+
+System.Console.WriteLine("Add Digit ");
+int digit = int.Parse(Console.ReadLine());
+int Sum = 0;
+while (digit>0)
+{
+    Sum += digit % 10;
+    digit /= 10;
+}
+
+System.Console.WriteLine("Sum of digits is equal "  + Sum );
+
+
+// Task 29
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8 };
+for (int i = 0; i <array.Length; i++)
+{
+    System.Console.WriteLine(array[i]);
+}
